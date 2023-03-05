6ec93acaa022144de62df7a1466d3db8cdec4507
SurenAvakian <Surenavakian96@gmail.com>
2023-03-05 20:06:17 +0200

    made a filter by using scope

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative '../query/filter'
 require_relative '../query/sort'
 
 # controller for cars
@@ -8,9 +7,6 @@ class CarsController < ApplicationController
   include Query
 
   def index
-    # @cars = Car.all
-    # filter = Query::Filter.new(@cars, params)
-    # @cars = filter.call
     @cars = Car.where(nil)
     filtering_params(params).each do |key, value|
       @cars = @cars.send("filter_by_#{key}", value) if value.present?
--- app/models/car.rb
+++ app/models/car.rb
@@ -10,10 +10,10 @@ class Car < ApplicationRecord
   scope :filter_by_make, -> (make) { where make: make.capitalize }
   scope :filter_by_model, -> (model) { where model: model.capitalize }
 
-  scope :filter_by_year_from, -> (year_from) { where year: year_from..year }
-  scope :filter_by_year_to, -> (year_to) { where year: year..year_to }
+  scope :filter_by_year_from, -> (year_from) { where year: year_from.to_i..9999 }
+  scope :filter_by_year_to, -> (year_to) { where year: (0..year_to.to_i) }
 
-  scope :filter_by_price_from, -> (price_from) { where price: price_from..price }
-  scope :filter_by_price_to, -> (price_to) { where price: price..price_to }
+  scope :filter_by_price_from, -> (price_from) { where price: (price_from.to_i..999_999) }
+  scope :filter_by_price_to, -> (price_to) { where price: (0..price_to.to_i) }
 
 end
