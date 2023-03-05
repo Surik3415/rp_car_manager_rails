ca9568ccacab06d4d0c1b0c446c9d362b0085bd9
SurenAvakian <Surenavakian96@gmail.com>
2023-03-05 14:00:14 +0200

    update car#index

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,13 +8,24 @@ class CarsController < ApplicationController
   include Query
 
   def index
-    @cars = Car.all
-    filter = Query::Filter.new(@cars, params)
-    @cars = filter.call
+    # @cars = Car.all
+    # filter = Query::Filter.new(@cars, params)
+    # @cars = filter.call
+    @cars = Car.where(nil)
+    filtering_params(params).each do |key, value|
+      @cars = @cars.send("filter_by_#{key}", value) if value.present?
+    end
     @pagy, @cars = pagy(@cars)
     sorter = Query::Sort.new(@cars, params)
     @cars = sorter.call
   end
 
   def help; end
+
+  private
+
+  # A list of the param names that can be used for filtering the Car list
+  def filtering_params(params)
+    params.slice(:make, :model)
+  end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -6,4 +6,8 @@ class Car < ApplicationRecord
   validates :model, presence: true
   validates :year, numericality: { only_integer: true }
   validates :price, numericality: { only_integer: true }
+
+  scope :filter_by_make, -> (make) { where make: make.capitalize }
+  scope :filter_by_model, -> (model) { where model: model.capitalize }
+
 end
